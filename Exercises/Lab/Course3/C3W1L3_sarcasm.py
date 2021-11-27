6302e625a969e96c493b51b294fbfd2b216e2605
Seojin Jang <82519477+SEOJIN-Lab@users.noreply.github.com>
2021-11-27 13:50:34 +0900

    Add files via upload

---
+++ Exercises/Lab/Course3/C3W1L3_sarcasm.py
@@ -0,0 +1,42 @@
+# @title Licensed under the Apache License, Version 2.0 (the "License"); { vertical-output: true }
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# sarcasm.json
+#!gdown --id 1xRU3xY5-tkiPGvlz5xBJ18_pHWSRzI4v
+
+import json
+
+with open("./sarcasm.json", 'r') as f:
+    datastore = json.load(f)
+
+sentences = []
+labels = []
+urls = []
+for item in datastore:
+    sentences.append(item['headline'])
+    labels.append(item['is_sarcastic'])
+    urls.append(item['article_link'])
+
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+tokenizer = Tokenizer(oov_token="<OOV>")
+tokenizer.fit_on_texts(sentences)
+
+word_index = tokenizer.word_index
+print(len(word_index))
+print(word_index)
+sequences = tokenizer.texts_to_sequences(sentences)
+padded = pad_sequences(sequences, padding='post')
+print(padded[0])
+print(padded.shape)
+
